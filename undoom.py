25c52a93c44558c3056d11048e85bb695f34d4a4
GnanambalM <127029974+GnanambalM@users.noreply.github.com>
2024-02-29 09:18:26 +0530

    partB_undoom.py

--- undoom.py
+++ undoom.py
@@ -1,42 +1,29 @@
-def calc_prob(die_a, die_b):
-    prob = {}
-    for face_a in die_a:
-        for face_b in die_b:
-            total = face_a + face_b
-            prob[total] = prob.get(total, 0) + 1
-    total_combinations = len(die_a) * len(die_b)
-    for key in prob:
-        prob[key] /= total_combinations
-    return prob
-def spots(die_a, die_b):
-    new_die_a = [min(4, x) for x in die_a]  
-    total_combinations = len(die_a) * len(die_b)
-    current_prob = calc_prob(die_a, die_b)
-    target_prob = calc_prob(new_die_a, die_b)   
-    for key in current_prob:
-        target_count = int(round(current_prob[key] * total_combinations))
-        current_count = int(round(target_prob.get(key, 0) * total_combinations))
-        diff = target_count - current_count
-        if diff > 0:
-            for i in range(1, 7):
-                if diff == 0:
-                    break
-                if i not in die_b:
-                    die_b.append(i)
-                    diff -= 1
-        elif diff < 0:
-            for i in range(1, 7):
-                if diff == 0:
-                    break
-                if i in die_b and i != 6:
-                    die_b.remove(i)
-                    diff += 1
-    return new_die_a, die_b
-def undoom_dice(die_a, die_b):
-    new_die_a, new_die_b = spots(die_a, die_b)
-    return new_die_a, new_die_b
-initial_die_a = [1, 2, 3, 4, 5, 6]
-initial_die_b = [1, 2, 3, 4, 5, 6]
-new_die_a, new_die_b = undoom_dice(initial_die_a,initial_die_b)
-print("New Die A:", new_die_a)
-print("New Die B:", new_die_b)
+from collections import Counter
+from itertools import product
+def undoom_dice(Die_A, Die_B):
+    def sum_of_dice(die_1, die_2):
+        return Counter(i + j for i, j in product(die_1, die_2))
+    normal_sum_dist = sum_of_dice(Die_A, Die_B)
+    def all_dice_comb():
+      dice_list = []
+      for x2 in range(2, 11):
+        for x3 in range(x2, 11):
+          for x4 in range(x3, 11):
+            for x5 in range(x4, 11):
+              for x6 in range(x5, 11):
+                dice_list.append([1, x2, x3, x4, x5, x6])
+      return dice_list
+    def find_modified_dice():
+        for die_1 in all_dice_comb():
+            for die_2 in all_dice_comb():
+                if (
+                    sum_of_dice(die_1, die_2) == normal_sum_dist
+                    and die_1 != Die_A
+                    and die_2 != Die_A
+                ):
+                    return die_1, die_2
+    modified_dice = find_modified_dice()
+    return modified_dice
+Die_A = [1, 2, 3, 4, 5, 6]
+Die_B = Die_A.copy()
+print(undoom_dice(Die_A, Die_B))
